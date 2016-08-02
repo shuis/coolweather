805306b4c00fa8af4581364fbf7dbb35ac4f9694
shuis <279669800@qq.com>
2016-08-02 13:59:49 +0800

    新增数据库帮助类，以及各表对应的实体类。

---
+++ app/src/main/java/com/example/coolweather/db/CoolWeatherOpenHelper.java
@@ -0,0 +1,36 @@
+package com.example.coolweather.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Administrator on 2016/8/2.
+ */
+public class CoolWeatherOpenHelper  extends SQLiteOpenHelper{
+    public static  final  String CREATE_PROVINCE ="create table province("+"id integer primary key autoincrement,"
+            +"province_name text,"
+            +"province_code text)";
+    public  static  final  String CREATE_CITY ="create table city ("+"id integer primary key autoincrement,"
+            +"city_name text,"
+            +"city_code text,"
+            +"province_id integer)";
+    public static final String CREATE_COUNTY="create table county("+"id integer primary key autoincrement,"
+            +"county_name text,"
+            +"county_code text,"
+            +"city_id integer）";
+    public CoolWeatherOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory,int version){
+        super(context,name,factory,version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_PROVINCE);
+        db.execSQL(CREATE_CITY);
+        db.execSQL(CREATE_COUNTY);
+    }
+    public void  onUpgrade(SQLiteDatabase db,int oldVersion,int newVersion)
+    {
+
+    }
+}
